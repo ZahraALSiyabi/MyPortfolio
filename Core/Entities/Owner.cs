599c8e0aca049283c8ecb92e207432533a67b3ff
ZahraALSiyabi <37467051+ZahraALSiyabi@users.noreply.github.com>
2020-06-28 11:41:08 +0400

    Add project files.

---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core.Entities
+{
+   public class Owner : EntityBase
+
+    {
+        public string FullName { get; set; }
+        public String Profil { get; set; }
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+    }
+}
